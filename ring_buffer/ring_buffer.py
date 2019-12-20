c2f381c2270fbe0942dad77926fa132abc43ca48
AlexFox1777 <mix.filipov@mail.ru>
2019-12-20 09:15:09 -0800

    ring_buffer passed tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,15 +8,41 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # if storage is empty define oldest value in self.current
+        if self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            if self.storage.length == 1:
+                self.current = self.storage.tail
+        # when storage equals to capacity overwrites the oldest value
+        elif self.storage.length == self.capacity:
+            self.current.value = item
+            # define new oldest item
+            if self.current.next:
+                self.current = self.current.next
+            else:
+                self.current = self.storage.head
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
+        current_node = self.storage.head
+        while current_node:
+            list_buffer_contents.append(current_node.value)
+            current_node = current_node.next
+        return list_buffer_contents
+
 
         # TODO: Your code here
 
         return list_buffer_contents
+
+
+ring = RingBuffer(3)
+ring.append('a')
+ring.append('b')
+ring.append('c')
+ring.append('d')
+
 
 # ----------------Stretch Goal-------------------
 
